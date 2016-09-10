dfea43ea08298d4340f2f74a0581be3e886f933d
ben <741763560@qq.com>
2016-09-10 10:16:02 +0800

    9-10-5

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,6 +2,6 @@
 return array(
 	//'配置项'=>'配置值'
     "RES_PATH" => "/attendance/Public",
-    "UPLOAD_ROOT" => "/var/www/upload",
-    "SAVE_ROOT" => "/var/www/save"
+    "UPLOAD_ROOT" => "/var/www/upload/",
+    "SAVE_ROOT" => "/var/www/save/"
 );
