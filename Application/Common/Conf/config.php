c681811509593361f264494223c08743d5cae98f
ben <741763560@qq.com>
2016-09-17 23:39:31 +0800

    路径更改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    "RES_PATH" => "/Public",
+    "RES_PATH" => "/Attendance/Public",
     "UPLOAD_ROOT" => "/var/www/upload/",
     "SAVE_ROOT" => "/var/www/save/"
 );
