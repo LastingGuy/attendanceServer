adadb01c9be92524e84a4677b4158e5cd768ffef
ben <741763560@qq.com>
2016-09-17 23:15:30 +0800

    release

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -56,8 +56,8 @@ return  array(
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'attendance',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '123456',          // 密码
+    'DB_USER'               =>  'attendance',      // 用户名
+    'DB_PWD'                =>  '1234598765',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'kaoqin_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
