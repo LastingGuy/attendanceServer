3344a152e1d09a77c91fba3108cf2a493f7a245c
ZhengChenYang <526789319@qq.com>
2016-09-28 20:03:31 +0800

    last

--- Application/Home/Controller/CourseController.class.php
+++ Application/Home/Controller/CourseController.class.php
@@ -41,8 +41,6 @@ class CourseController extends Controller
         $offset = I("get.offset");
         $limit = I("get.limit");
         $search = I("get.search");
-        echo $limit;
-        echo $search;
         if($search==""){
             $model = D("Course");
             $count = $model->count();
